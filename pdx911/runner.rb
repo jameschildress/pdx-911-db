aa69151f6e73ded762b633e8adff4cbb27b761f8
James Childress <james.childress@me.com>
2013-12-19 12:16:17 -0800

    updated runner and spec_runner requires

--- pdx911/runner.rb
+++ pdx911/runner.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-require_relative 'pdx911/pdx911'
+require_relative 'lib/pdx911'
 
-logger = Logger.new('pdx911.log', 10, 1024000)
+logger = Logger.new("#{__dir__}/pdx911.log", 10, 1024000)
 logger.level = Logger::INFO
 
 PDX911::Scraper.run logger
