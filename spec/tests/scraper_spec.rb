a96bbe16b0270f77aba21395e5c2930c0e3f9dc7
James Childress <james.childress@me.com>
2013-12-18 18:55:26 -0800

    passing spec for scraper

--- spec/tests/scraper_spec.rb
+++ spec/tests/scraper_spec.rb
@@ -75,6 +75,56 @@ describe PDX911::Scraper do
         PDX911::Dispatch.all(db).size.must_equal 4
       end
     end
+    
+    
+    
+    describe "when the feed has updated" do
+      
+      before do
+        scrape_sample_data :two
+        PDX911::Database.connect do |db|
+          @categories = PDX911::Category.all(db)
+          @agencies   = PDX911::Agency.all(db)
+          @dispatches = PDX911::Dispatch.all(db)
+        end
+      end
+      
+      it "creates a Category for every new category" do
+        @categories.size.must_equal 3
+        @categories[0].name.must_equal 'UNWANTED PERSON'
+        @categories[1].name.must_equal 'THEFT - COLD'
+        @categories[2].name.must_equal 'HAZARD - HAZARDOUS CONDITION'
+      end
+
+      it "creates an Agency for every new agency" do
+        @agencies.size.must_equal 2
+        @agencies[0].name.must_equal 'Portland Police'
+        @agencies[1].name.must_equal 'Gresham Police'
+      end
+      
+      it "creates a Dispatch for every new entry" do
+        @dispatches.size.must_equal 6
+        @dispatches[0].uid.must_equal         'PP13000410538'
+        @dispatches[1].uid.must_equal         'PG13000069877'
+        @dispatches[2].uid.must_equal         'PP13000410477'
+        @dispatches[3].uid.must_equal         'PP13000410491'
+
+        @dispatches[4].uid.must_equal         'PG13000069883'
+        @dispatches[4].address.must_equal     '0 NE BURNSIDE RD, GRESHAM, OR'
+        @dispatches[4].category_id.must_equal  @categories[0].id
+        @dispatches[4].agency_id.must_equal    @agencies[1].id
+        @dispatches[4].location.must_equal    '(45.508433,-122.430623)'
+        DateTime.parse(@dispatches[4].date).must_equal DateTime.parse('2013-12-16T16:12:04.0-08:00')
+
+        @dispatches[5].uid.must_equal         'PP13000410551'
+        @dispatches[5].address.must_equal     'SE 112TH AVE / SE HOLGATE BLVD, PORTLAND, OR'
+        @dispatches[5].category_id.must_equal  @categories[2].id
+        @dispatches[5].agency_id.must_equal    @agencies[0].id
+        @dispatches[5].location.must_equal    '(45.489758,-122.548001)'
+        DateTime.parse(@dispatches[5].date).must_equal DateTime.parse('2013-12-16T16:09:22.0-08:00')
+      end
+      
+    end
 
 
     
